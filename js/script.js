24d1c3540d7f5427d788177121862e2467440fba
Lucas <lucasmatiasblanco@outlook.com>
2021-09-15 16:29:58 -0300

    all the functionality has been redone

--- js/script.js
+++ js/script.js
@@ -8,6 +8,11 @@
     }  
 */
 
+
+
+
+
+/*
 const addButton = document.querySelector('.addButton')
 const closeWindow = document.querySelector('.closeWindow')
 const body = document.querySelector('body');
@@ -37,8 +42,7 @@ submitButton.addEventListener('click', saveBook)
 
 function saveBook() {
     setValue();
-    getValue();
-    
+    //getValue();
 }
 
 function setValue() {
@@ -46,6 +50,7 @@ function setValue() {
     localStorage.setItem('author', document.getElementById('author').value)
     localStorage.setItem('pages', document.getElementById('pages').value);
     localStorage.setItem('readStatus', document.getElementById('readStatus').checked)
+    getValue();
 }
 
 let saveTitle;
@@ -80,4 +85,161 @@ Libro.prototype.showBook = function() {
 }
 
 allNewBook.prototype = Object.create(Libro.prototype);
+*/
+
+/// 
+
+let myLibrary = [];
+
+const titleForm = document.getElementById('title');
+const authorForm = document.getElementById('author');
+const pagesForm = document.getElementById('pages');
+const readStatus = document.getElementById('readStatus');
+
+
+
+function newBook(){
+}
+
+function Book(title,author,pages,read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read; 
+}
+
+
+Book.prototype = Object.create(newBook.prototype);
+
+/*
+newBook.prototype.addBookToLibrary = function() {
+  myLibrary.push(this.Book);
+}
+*/
+
+function addBookToLibrary(book) {
+    myLibrary.push(book);
+}
+
+const userInput = document.querySelectorAll('.container_input'); 
+const submitButton = document.querySelector('#container_submit_button'); 
+
+submitButton.addEventListener('click', showUserInput)
+
+let userValues = [];
+
+
+
+function showUserInput() {
+
+    let titleValue = titleForm.value;
+    let authorValue = authorForm.value;
+    let pagesValue = pagesForm.value;
+    let readValue = readStatus.checked;
+
+    
+    if(readValue === true) readValue = 'Read'
+    else readValue = 'Unread';
+
+    const addBook = new Book(titleValue, authorValue, pagesValue, readValue)
+    myLibrary.push(addBook);
+
+
+    const body = document.querySelector('body')
+    const newDiv = document.createElement('div')
+    newDiv.setAttribute('class', 'user_text')
+    
+/*
+    myLibrary.forEach( book => {
+        Object.values(book).forEach( categorie => {
+        let information = document.createElement('span');
+        information.setAttribute('class','book_information');
+        information.textContent = categorie;
+        newDiv.appendChild(information);
+        }
+        )
+    })
+    body.appendChild(newDiv);
+
+*/
+
+
+    myLibrary.forEach( (book,index,array) => {
+        if(index === array.length - 1) 
+        Object.values(book).forEach( categorie => {
+            if(categorie === 'read') {
+                let buttonReadStatus = document.createElement('input')
+                buttonReadStatus.setAttribute('type', 'checkbox');
+                newDiv.appendChild(buttonReadStatus);
+            }
+        let information = document.createElement('div');
+        information.setAttribute('class','book_information');
+       
+        information.textContent = categorie;
+        newDiv.appendChild(information);
+        }
+        )
+    })
+    body.appendChild(newDiv);
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /*
+
+
+
+
+    for (let i = 0; i < myLibrary.length; i++) {
+        for (const value of Object.entries(myLibrary[i])) {
+            console.log(value);
+          }
+    }
+
+*/
+
+
+/*
+    const body = document.querySelector('body')
+    const newDiv = document.createElement('div')
+    newDiv.setAttribute('class', 'user_text')
+    body.appendChild(newDiv);
+    newDiv.textContent = `TITLE: ${addBook.title}, AUTHOR: ${addBook.author}, PAGES: ${addBook.pages}, HAS BEEN READ?: ${addBook.read}`;
+    
+*/  
+    
+    
+    
+    /*yLibrary[0]
+     userValues = []; 
+    storageValues();
+    const body = document.querySelector('body')
+    const newDiv = document.createElement('div')
+    newDiv.setAttribute('class', 'user_text')
+    body.appendChild(newDiv);
+    newDiv.textContent = userValues;
+    */
+}
+
+
+/*
+function storageValues() {
+    for(let i = 0; i < userInput.length; i++) {
+        userValues.push(userInput[i].value);
+    }
+}
+*/
+
+function storageValues() {
+    Array.from(userInput).map( item => userValues.push(item.value));
+}
 
