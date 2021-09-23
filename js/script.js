e5fea007bae7692b8460b8f3d1b1e75e4fc0086d
Lucas <lucasmatiasblanco@outlook.com>
2021-09-23 12:01:51 -0300

    added Css styling

--- js/script.js
+++ js/script.js
@@ -1,95 +1,10 @@
-/*function books(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    this.info = function() {
-        return `${title} by ${author}, ${pages} pages, ${read}`; 
-    }  
-*/
+let myLibraryStorage = [];
 
+window.onload = loadBooks();
 
-
-
-
-/*
-const addButton = document.querySelector('.addButton')
-const closeWindow = document.querySelector('.closeWindow')
-const body = document.querySelector('body');
-const informationField = document.querySelector('.informationField')
-
-
-addButton.addEventListener('click', newBook)
-function newBook () {
-    informationField.setAttribute("style", "display: block");
-
+function loadBooks() {
+    myLibraryStorage = JSON.parse(localStorage.getItem('bookStorage') || '[]');
 }
-
-closeWindow.addEventListener('click', closeForm)
-function closeForm() {
-    informationField.setAttribute("style", "display: none");
-}
-
-///
-
-const titleForm = document.getElementById('title');
-const authorForm = document.getElementById('author');
-const pagesForm = document.getElementById('pages');
-const readStatus = document.getElementById('readStatus');
-const submitButton = document.getElementById('storageData');
-
-submitButton.addEventListener('click', saveBook)
-
-function saveBook() {
-    setValue();
-    //getValue();
-}
-
-function setValue() {
-    localStorage.setItem('title', document.getElementById('title').value)
-    localStorage.setItem('author', document.getElementById('author').value)
-    localStorage.setItem('pages', document.getElementById('pages').value);
-    localStorage.setItem('readStatus', document.getElementById('readStatus').checked)
-    getValue();
-}
-
-let saveTitle;
-let saveAuthor;
-let savePages;
-let saveReadStatus;
-
-function getValue() {
-    saveTitle = localStorage.getItem('title');
-    saveAuthor = localStorage.getItem('author');
-    savePages = localStorage.getItem('pages')
-    saveReadStatus = localStorage.getItem('readStatus');
-}
-
-
-
-///
-
-
-let library = []; 
-
-function Libro() {}
-
-function allNewBook(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read; 
-}  
-
-Libro.prototype.showBook = function() {
-    console.log(`title: ${this.title}, author: ${this.author}, pages: ${this.pages}, read: ${this.read}`);
-}
-
-allNewBook.prototype = Object.create(Libro.prototype);
-*/
-
-/// 
-
   
 
 
@@ -116,14 +31,13 @@ function Book(title,author,pages,read) {
 Book.prototype = Object.create(newBook.prototype);
 
 
-
-
 function addBookToLibrary(book) {
     myLibrary.push(book);
+    localStorage.setItem('bookStorage', JSON.stringify(book));
 }
 
 const userInput = document.querySelectorAll('.container_input'); 
-const submitButton = document.querySelector('#container_submit_button'); 
+const submitButton = document.querySelector('.container_submit_button'); 
 
 submitButton.addEventListener('click', showUserInput)
 
@@ -148,16 +62,17 @@ function showUserInput() {
         return;
     }
 
-    myLibrary.push(addBook);
+    addBookToLibrary(addBook);
 
 
+   
+ 
 
 
-    const body = document.querySelector('body')
+    const libraryContainer = document.getElementById('container_library');
     const newDiv = document.createElement('div')
     newDiv.setAttribute('class', 'user_text')
     
-
 
     myLibrary.forEach((book,index,array) => {
         newDiv.setAttribute('data-book-number', index);
@@ -177,16 +92,24 @@ function showUserInput() {
                 let readState = document.createElement('button');
                 readState.setAttribute('class', 'readConfirmation');
                 readState.textContent = readValue; 
+                if(categorie === 'Read') {
+                    readState.innerHTML = '<i class="material-icons">visibility</i>'
+             } else {
+                 readState.innerHTML = '<i class="material-icons">visibility_off</i>'
+            }
                 newDiv.appendChild(readState);
                 information.replaceWith(readState);
 
                 readState.addEventListener('click', changeReadState) 
 
                 function changeReadState() {
-                    if(readState.textContent === 'Read') {
-                        readState.textContent = 'Unread';
+                    if(readState.innerHTML  === '<i class="material-icons">visibility</i>') {
+                        //readState.textContent = 'Unread';
+                        readState.innerHTML = '<i class="material-icons">visibility_off</i>'
                         myLibrary[index].read = 'Unread';
-                    } else { readState.textContent = 'Read'; 
+                    } else { 
+                    readState.innerHTML = '<i class="material-icons">visibility</i>'
+                    // readState.textContent = 'Read'; 
                     myLibrary[index].read = 'Read';
                 }
                 }
@@ -212,11 +135,42 @@ function showUserInput() {
 
         }
     })
-    body.appendChild(newDiv);
+    libraryContainer.appendChild(newDiv);
+
+    titleForm.value = '';
+    authorForm.value = '';
+    pagesForm.value = '';
+    readStatus.checked = false;
+
+
+}
+
+/////////
+
+const buttonAddBook = document.getElementById('button_addBook');
+const containerForm = document.querySelector('.container');
+
+buttonAddBook.addEventListener('click', seeForm);
+
+
+function seeForm() {
+    containerForm.style.display = 'flex';
+
+}
+
+const cancelForm = document.querySelector('.container_cancel_form');
+
+cancelForm.addEventListener('click', formIsNoMore)
+
+function formIsNoMore() {
+
+    titleForm.value = '';
+    authorForm.value = '';
+    pagesForm.value = '';
+    readStatus.checked = false;
+
+    containerForm.style.display = 'none';
 }
 
 
-
-
-
-
+let almacenadoLibros = JSON.parse(localStorage.getItem("bookStorage") || "[]");
